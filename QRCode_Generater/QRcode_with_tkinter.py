0ffdc4c50dfdc400e10991d06a60e30de5e80341
palakgoel30 <gpalak7055@gmail.com>
2023-09-12 21:41:23 +0530

    modified file

--- QRCode_Generater/QRcode_with_tkinter.py
+++ QRCode_Generater/QRcode_with_tkinter.py
@@ -3,15 +3,23 @@ import qrcode
 
 window = Tk()
 window.title("QRCode Generation")
-window.minsize(width = 500, height=600)
-window.maxsize(width = 800, height = 800)
+window.minsize(width=500, height=600)
+window.maxsize(width=800, height=800)
 
-link = input("Enter url link for which you want to create a QRCode")
+l1 = Label(window, text = 'Enter url link for which you want to create a QRCode')
+l1.place(x= 5,y=7)
+#link = input("Enter url link for which you want to create a QRCode")
+e1 = Entry(window,width = 20, bd=1)
+e1.place(x=5,y=25)
+
+b1 = Button(window, text="Submit")
+b1.place(x=150, y=80)
+
 features = qrcode.QRCode(version=1, box_size=40, border=2)
-features.add_data(link)
+features.add_data(e1)
 features.make(fit=True)
 generate_image1 = features.make_image(fill_color="red", back_color='white')
-generate_image1.save('link.png')
+generate_image1.save('link1.png')
 
 generate_image2 = qrcode.make("sample sample")
 generate_image2.save('sample.png')
