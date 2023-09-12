693eabf77050cde04d256ae1d6de519c922a93c0
palakgoel30 <gpalak7055@gmail.com>
2023-09-12 20:53:07 +0530

    Add Tkinter for UI

--- QRCode_Generater/QRcode_with_tkinter.py
+++ QRCode_Generater/QRcode_with_tkinter.py
@@ -1,5 +1,10 @@
+from tkinter import *
 import qrcode
 
+window = Tk()
+window.title("QRCode Generation")
+window.minsize(width = 500, height=600)
+window.maxsize(width = 800, height = 800)
 
 link = input("Enter url link for which you want to create a QRCode")
 features = qrcode.QRCode(version=1, box_size=40, border=2)
@@ -10,3 +15,5 @@ generate_image1.save('link.png')
 
 generate_image2 = qrcode.make("sample sample")
 generate_image2.save('sample.png')
+
+window.mainloop()
