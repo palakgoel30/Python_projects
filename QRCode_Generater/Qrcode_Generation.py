e677303aa9bb3bc4ee8618d5a5e363540d257483
palakgoel30 <gpalak7055@gmail.com>
2023-09-12 16:13:38 +0530

    Add one feature

--- QRCode_Generater/Qrcode_Generation.py
+++ QRCode_Generater/Qrcode_Generation.py
@@ -1,7 +1,8 @@
 import qrcode
 
+link = input("Enter url link for which you want to create a QRCode")
 features = qrcode.QRCode(version=1, box_size=40, border=2)
-features.add_data('https://github.com/palakgoel30/Snake_Game')
+features.add_data(link)
 features.make(fit=True)
 generate_image1 = features.make_image(fill_color="red", back_color='white')
 generate_image1.save('link.png')
